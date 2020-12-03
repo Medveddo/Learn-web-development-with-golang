55b3de01ca38aff85013ec66799dc18a4576284e
Vitaly Sizikov <606er.54rus@gmail.com>
2020-12-03 15:19:46 +0700

    Created a '/contact' page and 404-page

--- main.go
+++ main.go
@@ -6,7 +6,18 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my page!<h1/>")
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my cool page!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "Text me on +7-903-997-37-72<br>Or send an email to sizikov.vitaly@gmail.com")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>We could not find the page you "+
+			"were looking for :(</h1>"+
+			"<p>Please email me if you keep being sent to an "+
+			"invalid page </p>")
+	}
 }
 
 func main() {
