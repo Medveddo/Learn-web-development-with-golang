c675b27fcefc31e9c540d5be8ca9bec72bb0a0b8
Vitaly Sizikov <606er.54rus@gmail.com>
2020-12-07 14:27:50 +0700

    Create the same app that in 3.4 but using julienschmidt/httprouter

--- main.go
+++ main.go
@@ -2,26 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
-	"github.com/gorilla/mux/"
+	"github.com/julienschmidt/httprouter"
 )
 
 /*
 	Block of handle function. Each of them give specific page.
 */
-func home(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to my cool page!</h1>")
 }
 
-func contact(w http.ResponseWriter, r *http.Request) {
+func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Contacts</h1>For any questions.<br><b>Phone:</b>+7-903-997-37-72<br>"+
 		"<br><b>Email:</b>sizikov.vitaly@gmail.com")
 }
 
-func faq(w http.ResponseWriter, r *http.Request) {
+func faq(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>FAQ</h1><ul><li><b>What a hell are doing?</b><p>"+
 		"I'm try to build a web application usign Golang.</p></li><li><b>Is that legal?</b>"+
@@ -37,13 +38,14 @@ func pageNotFound(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	r := mux.NewRouter()
+	//Creating a router
+	router := httprouter.New()
+	// Set the handler functions to different URL's
+	router.GET("/", home)
+	router.GET("/contact", contact)
+	router.GET("/faq", faq)
 	// Set the custom 404 page instead of deafult.
 	var h http.Handler = http.HandlerFunc(pageNotFound)
-	r.NotFoundHandler = h
-	// Set the handler functions to different URL's
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", faq)
-	http.ListenAndServe(":3000", r)
+	router.NotFound = h
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
