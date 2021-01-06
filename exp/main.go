841f67cf0143bb97cd9f12e275fc383616802b78
Vitaly Sizikov <606er.54rus@gmail.com>
2021-01-06 15:14:18 +0700

    Experiment with Go templates. Play with nested structures, maps (just new in Go, learn how to work with them.

--- exp/main.go
+++ exp/main.go
@@ -1,27 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"math"
 	"os"
 )
 
-// Character is character struct
+type Inventory struct {
+	Size int
+}
+
 type Character struct {
-	Name   string
-	HP     uint
-	Attack uint
+	Name    string
+	HP      uint
+	Attack  uint
+	Boolean bool
+	FLvalue float64
+	Arr     []int
+	Map     map[string]string
+	Invent  Inventory
 }
 
 func main() {
+	var pi = math.Pi
 	t, err := template.ParseFiles("template.gohtml")
 	if err != nil {
 		panic(err)
 	}
 
+	inv := Inventory{Size: 33}
 	var data Character
-	data.Name = "<script>alert('Howdy!');</script>"
-	data.HP = 100
-	data.Attack = 15
+	data = Character{
+		Name:    "Vitaly",
+		HP:      100,
+		Attack:  25,
+		Boolean: true,
+		FLvalue: pi,
+		Arr:     []int{10, 20, 30},
+		Map: map[string]string{
+			"key1": "value1", "key2": "value2",
+		},
+		Invent: inv,
+	}
+
+	fmt.Println(data.Arr)
 
 	err = t.Execute(os.Stdout, data)
 	if err != nil {
