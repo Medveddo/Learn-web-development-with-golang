e2b6987668fbf530125c94d00e7c398aff9cf842
Vitaly Sizikov <606er.54rus@gmail.com>
2021-03-05 14:59:07 +0700

    Created contact view

--- main.go
+++ main.go
@@ -7,7 +7,10 @@ import (
 	"github.com/gorilla/mux/"
 )
 
-var homeTemplate *template.Template
+var (
+	homeTemplate    *template.Template
+	contactTemplate *template.Template
+)
 
 /*
 	Block of handle function. Each of them give specific page.
@@ -19,6 +22,14 @@ func home(w http.ResponseWriter, r *http.Request) {
 	if err := homeTemplate.Execute(w, nil); err != nil {
 		panic(err)
 	}
+
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
@@ -27,7 +38,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
+	if err != nil {
+		panic(err)
+	}
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
 	http.ListenAndServe(":3000", r)
 }
