86a0d0dd16a46e0151af14638f5c717cd19d4836
Vitaly Sizikov <606er.54rus@gmail.com>
2021-03-07 16:12:57 +0700

    Cleaned up views.go Now all .gohtml files in views/layouts/ directory automaticaly append to the files list

--- views/views.go
+++ views/views.go
@@ -1,13 +1,17 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"path/filepath"
+)
+
+var (
+	LayoutDir   string = "views/layouts/"
+	TemplateExt string = ".gohtml"
+)
 
 func NewView(layout string, files ...string) *View {
-	files = append(files,
-		"views/layouts/bootstrap.gohtml",
-		"views/layouts/navbar.gohtml",
-		"views/layouts/footer.gohtml",
-	)
+	files = append(files, layoutFiles()...)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
@@ -23,3 +27,19 @@ type View struct {
 	Template *template.Template
 	Layout   string
 }
+
+// not capital letter because we dont wanna export this function out of package
+// returns a slice of strings representing the layout used in our application
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
+}
+
+/* Return example
+"views/layouts/bootstrap.gohtml"
+"views/layouts/navbar.gohtml"
+"views/layouts/footer.gohtml"
+*/
