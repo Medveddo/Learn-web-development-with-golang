3cc7c751e571825647bd07e21579ad012e7984c9
Vitaly Sizikov <606er.54rus@gmail.com>
2021-03-08 15:10:13 +0700

    Created a signup page(view)

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ var (
 	homeView    *views.View
 	contactView *views.View
 	faqView     *views.View
+	signupView  *views.View
 )
 
 /*
@@ -32,15 +33,22 @@ func faq(w http.ResponseWriter, r *http.Request) {
 	must(faqView.Render(w, nil))
 }
 
+func signup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(signupView.Render(w, nil))
+}
+
 func main() {
 	// Creating our views
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 	faqView = views.NewView("coverLayout", "views/faq.gohtml")
+	signupView = views.NewView("bootstrap", "views/signup.gohtml")
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
+	r.HandleFunc("/signup", signup)
 	http.ListenAndServe(":3000", r)
 }
 
