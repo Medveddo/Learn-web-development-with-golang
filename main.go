9d8b619d8a79925bbb7cd5faa960c244c2d47976
Vitaly Sizikov <606er.54rus@gmail.com>
2021-03-08 14:44:42 +0700

    Created a new layout and use it on faq page

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func main() {
 	// Creating our views
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
-	faqView = views.NewView("bootstrap", "views/faq.gohtml")
+	faqView = views.NewView("coverLayout", "views/faq.gohtml")
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
