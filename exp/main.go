e5a7cb6a45da47cdc22069187f617e15404ef324
Vitaly Sizikov <606er.54rus@gmail.com>
2021-03-26 23:57:41 +0700

    Successifully added records to database

--- exp/main.go
+++ exp/main.go
@@ -17,7 +17,7 @@ const (
 
 func main() {
 	//psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-	//	host, port, user, password, dbname) cause an error at db.Ping()
+	//	host, port, user, password, dbname) cause an error at db.Ping() pord=%d !!!! ERROR)))
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
@@ -25,10 +25,33 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	err = db.Ping()
+	defer db.Close()
+
+	var id int
+
+	err = db.QueryRow(`
+		INSERT INTO  users(name, email)
+		VALUES($1,$2)
+		RETURNING id`,
+		"Kek Ivanovich",
+		"kek.lol@gmail.com").Scan(&id)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Connected!")
+	fmt.Println("Inserted user's id: ", id)
 	db.Close()
+	/*
+		C:\Go\src\MyWebApp\exp>go run main.go
+		Inserted user's id:  2
+
+		C:\Go\src\MyWebApp\exp>go run main.go
+		Inserted user's id:  3
+
+		mywebapp_dev=# SELECT * from users;
+		id |      name      |          email
+		----+----------------+--------------------------
+		1 | Vitaly Sizikov | vitaly.sizikov@gmail.com
+		2 | Vitaly Sizikov | vitaly.sizikov@gmail.com
+		3 | Kek Ivanovich  | kek.lol@gmail.com
+	*/
 }
