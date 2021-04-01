0759c3758e888033713c782822036e19568ad8db
Vitaly Sizikov <606er.54rus@gmail.com>
2021-04-01 20:52:25 +0700

    Successfully got GORM package by go get and connect our exp/main.go code to a database

--- exp/main.go
+++ exp/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 
-	_ "github.com/lib/pq"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 const (
@@ -16,53 +16,15 @@ const (
 )
 
 func main() {
-	//psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-	//	host, port, user, password, dbname) cause an error at db.Ping() pord=%d !!!! ERROR)))
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
-
-	/* INSERTING SOME VALUES
-	for i := 1; i <= 6; i++ {
-		userID := 1
-		if i > 3 {
-			userID = 3
-		}
-		amount := i * 100
-		description := fmt.Sprintf("USB-C Adapter x%d", i)
-
-		_, err = db.Exec(`
-		INSERT INTO orders(user_id,amount,description)
-		VALUES ($1, $2, $3)`, userID, amount, description)
-
-		if err != nil {
-			panic(err)
-		}
-	}
-	*/
-
-	rows, err := db.Query(`
-	SELECT *
-	FROM users
-	INNER JOIN orders  ON users.id = orders.user_id`)
-	if err != nil {
-		panic(err)
-	}
-	for rows.Next() {
-		var userID, orderID, amount int
-		var email, name, desc string
-		if err := rows.Scan(&userID, &name, &email, &orderID, &userID, &amount, &desc); err != nil {
-			panic(err)
-		}
-		fmt.Println("userID:", userID, "name:", name, "email:", email,
-			"orderID:", orderID, "amount:", amount, "desc:", desc)
-	}
-	if rows.Err() != nil {
+	if err = db.DB().Ping(); err != nil {
 		panic(err)
 	}
 }
