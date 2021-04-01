0ca224fd72bfe467361fec048428dff68208ecbf
Vitaly Sizikov <606er.54rus@gmail.com>
2021-04-01 23:04:40 +0700

    Queried records using Not(), Where() and Or() functions

--- exp/main.go
+++ exp/main.go
@@ -17,56 +17,47 @@ const (
 
 type User struct {
 	gorm.Model
-	Name   string
-	Email  string `gorm:"not null;unique_index"`
-	Color  string
-	Orders []Order
-}
-
-type Order struct {
-	gorm.Model
-	UserID      uint
-	Amount      int
-	Description string
+	Name  string
+	Email string `gorm:"not null;unique_index"`
+	Color string
 }
 
 func main() {
+	// Creating our database info string which will be passed to gorm.Open function and has all information needed to get successful connection
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
+	// Connecting to our database
 	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
-	db.LogMode(true)
-	db.AutoMigrate(&User{}, &Order{})
+	db.LogMode(true)        // Enable logging to see all SQL queries underlying
+	db.AutoMigrate(&User{}) // Creating a users table if it doesn't exist or modify it
 
-	// var u User
-	// if err := db.Preload("Orders").Where("color = ?", "Red").First(&u).Error; err != nil {
+	// Adding some records
+	// db.Create(&User{Name: "Sizikov Vitaly", Email: "sizikov.vitaly@gmail.com", Color: "blue"})
+	// db.Create(&User{Name: "Dasha Malko", Email: "malkodasha25@gmail.com", Color: "green"})
+	// db.Create(&User{Name: "Sergey Kolmogorov", Email: "kekivan@gmail.com", Color: "black"})
+
+	// Querying records using Not() function
+	// var users []User
+	// if err := db.Not("color = ?", "green").Find(&users).Error; err != nil {
 	// 	panic(err)
 	// }
-	//fmt.Println(u)
+	// fmt.Println(users)
+
+	// Querying records using Where() and Or() function
+	// var users []User
+	// if err := db.Where("name = ?", "Sizikov Vitaly").Or("color = ?", "green").Find(&users).Error; err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(users)
 
 	var users []User
-	if err := db.Preload("Orders").Find(&users).Error; err != nil {
-		panic(nil)
-	}
-	fmt.Println(users)
-
-	// createOrder(db, u, 1001, "Fake description #1")
-	// createOrder(db, u, 9999, "Fake description #2")
-	// createOrder(db, u, 100, "Fake description #3")
-
-}
-
-func createOrder(db *gorm.DB, user User, amount int, desc string) {
-	err := db.Create(&Order{
-		UserID:      user.ID,
-		Amount:      amount,
-		Description: desc,
-	}).Error
-	if err != nil {
+	if err := db.Where("name LIKE ?", "%gey%").Find(&users).Error; err != nil {
 		panic(err)
 	}
+	fmt.Println(users)
 }
