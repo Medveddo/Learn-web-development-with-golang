98e42696a000e917a5af60c11ec24e416954fe34
Vitaly Sizikov <606er.54rus@gmail.com>
2021-04-04 14:49:48 +0700

    Created the UserService for interacting with user model

--- exp/main.go
+++ exp/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
+	"learn-web-dev-with-go/models"
 
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
@@ -15,49 +15,20 @@ const (
 	dbname   = "mywebapp_dev"
 )
 
-type User struct {
-	gorm.Model
-	Name  string
-	Email string `gorm:"not null;unique_index"`
-	Color string
-}
-
 func main() {
 	// Creating our database info string which will be passed to gorm.Open function and has all information needed to get successful connection
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
-	// Connecting to our database
-	db, err := gorm.Open("postgres", psqlInfo)
+	us, err := models.NewUserService(psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
-	db.LogMode(true)        // Enable logging to see all SQL queries underlying
-	db.AutoMigrate(&User{}) // Creating a users table if it doesn't exist or modify it
-
-	// Query Raw SQL with Scan
-	// type Result struct {
-	// 	ID    int
-	// 	Name  string
-	// 	Email string
-	// 	Color string
-	// }
-	// var result Result
-	// db.Raw("SELECT id, name, email, color FROM users WHERE id = ?", 2).Scan(&result)
-	// fmt.Printf("%+v\n", result)
-	// {ID:2 Name:Dasha Malko Email:malkodasha25@gmail.com Color:green}
-
-	// Query getting result as *sql.Row
-	// var name, email string
-	// row := db.Raw("SELECT name, email from USERS where name = ?", "Sizikov Vitaly").Row()
-	// row.Scan(&name, &email)
-	// fmt.Println(name, email)
-	// Sizikov Vitaly sizikov.vitaly@gmail.com
-
-	// Update Raw SQL
-	db.Exec("UPDATE users SET name = ? WHERE id = ?", "Vitaly Sizikov", 1)
-	var u User
-	db.Where("id = ?", 1).First(&u)
-	fmt.Println(u)
+	defer us.Close()
+	//us.DestructiveReset()
+	user, err := us.ByID(2)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(user)
 }
--- models/users.go
+++ models/users.go
@@ -1,12 +1,64 @@
 package models
 
 import (
-  "github.com/jinzhu/gorm"
-  _ "github.com/jinzhu/gorm/dialects/postgres"
+	"errors"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var (
+	// ErrNotFound is returned when a recource cannot be found
+	// in the database.
+	ErrNotFound = errors.New("models: resource not found")
 )
 
 type User struct {
-  gorm.Model
-  Name string
-  Email string `gorm:"not null;unique_index"`
+	gorm.Model
+	Name  string
+	Email string `gorm:"not null;unique_index"`
+}
+
+type UserService struct {
+	db *gorm.DB
+}
+
+func NewUserService(connectionInfro string) (*UserService, error) {
+	// Connecting to our database
+	db, err := gorm.Open("postgres", connectionInfro)
+	if err != nil {
+		return nil, err
+	}
+	db.LogMode(true)
+	return &UserService{
+		db: db,
+	}, nil
+}
+
+// ByID will look up the user by id provided
+// 1 - user, nil
+// 2 - nil, ErrNotFound
+// 3 - nil, otherError
+func (us *UserService) ByID(id uint) (*User, error) {
+	var user User
+	err := us.db.Where("id = ?", id).First(&user).Error
+	switch err {
+	case nil:
+		return &user, nil
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+	}
+}
+
+// Closes the UserService database connection
+func (us *UserService) Close() error {
+	return us.db.Close()
+}
+
+// Drops a users table and rebuilds it
+func (us *UserService) DestructiveReset() {
+	us.db.DropTableIfExists(&User{})
+	us.db.AutoMigrate(&User{})
 }
