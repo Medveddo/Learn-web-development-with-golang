06199c9632630cbc4c6facf74acf298d06cace07
Vitaly Sizikov <606er.54rus@gmail.com>
2021-04-24 21:33:48 +0700

    Implemented error handling for render errors

--- controllers/users.go
+++ controllers/users.go
@@ -32,10 +32,7 @@ type Users struct {
 //
 // GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-	if err := u.NewView.Render(w, nil); err != nil {
-		panic(err)
-	}
-
+	u.NewView.Render(w, nil)
 }
 
 // SignupForm is a struct that contains data
--- views/views.go
+++ views/views.go
@@ -1,7 +1,9 @@
 package views
 
 import (
+	"bytes"
 	"html/template"
+	"io"
 	"net/http"
 	"path/filepath"
 )
@@ -34,13 +36,11 @@ type View struct {
 }
 
 func (v *View) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if err := v.Render(w, nil); err != nil {
-		panic(err)
-	}
+	v.Render(w, nil)
 }
 
 //Render is used to render the view with the predefined layout
-func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+func (v *View) Render(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "text/html")
 	switch data.(type) {
 	case Data:
@@ -50,7 +50,12 @@ func (v *View) Render(w http.ResponseWriter, data interface{}) error {
 			Yield: data,
 		}
 	}
-	return v.Template.ExecuteTemplate(w, v.Layout, data)
+	var buf bytes.Buffer
+	if err := v.Template.ExecuteTemplate(&buf, v.Layout, data); err != nil {
+		http.Error(w, "Something went wrong. If the problem presists, please email support@futurecycling.space", http.StatusInternalServerError)
+		return
+	}
+	io.Copy(w, &buf)
 }
 
 // not capital letter because we dont wanna export this function out of package
