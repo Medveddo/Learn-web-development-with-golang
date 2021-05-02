b7127e19ddd008a895fa0751e50a50c872769ee5
Vitaly Sizikov <606er.54rus@gmail.com>
2021-05-02 23:04:15 +0700

    Added a delete action to the galleries controller and Gallery model

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"learn-web-dev-with-go/context"
 	"learn-web-dev-with-go/models"
 	"learn-web-dev-with-go/views"
@@ -128,6 +129,29 @@ func (g *Galleries) Create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	http.Redirect(w, r, url.Path, http.StatusFound)
+}
+
+// POST /galleries/:id/delete
+func (g *Galleries) Delete(w http.ResponseWriter, r *http.Request) {
+	gallery, err := g.galleryByID(w, r)
+	if err != nil {
+		return
+	}
+	user := context.User(r.Context())
+	if gallery.UserID != user.ID {
+		http.Error(w, "Gallery not found", http.StatusNotFound)
+		return
+	}
+	var vd views.Data
+	err = g.gs.Delete(gallery.ID)
+	if err != nil {
+		vd.SetAlert(err)
+		vd.Yield = gallery
+		g.EditView.Render(w, vd)
+		return
+	}
+	// TODO: Redirect to index page
+	fmt.Fprintln(w, "successfully deleted!")
 }
 
 func (g *Galleries) galleryByID(w http.ResponseWriter, r *http.Request) (*models.Gallery, error) {
--- models/galleries.go
+++ models/galleries.go
@@ -53,6 +53,13 @@ func (gv *galleryValidator) Update(gallery *Gallery) error {
 	return gv.GalleryDB.Update(gallery)
 }
 
+func (gv *galleryValidator) Delete(id uint) error {
+	if id <= 0 {
+		return ErrIDInvalid
+	}
+	return gv.GalleryDB.Delete(id)
+}
+
 func (gv *galleryValidator) userIDRequired(g *Gallery) error {
 	if g.UserID <= 0 {
 		return ErrUserIDRequired
@@ -82,6 +89,7 @@ type GalleryDB interface {
 	ByID(id uint) (*Gallery, error)
 	Create(gallery *Gallery) error
 	Update(gallery *Gallery) error
+	Delete(id uint) error
 }
 
 type galleryGorm struct {
@@ -96,6 +104,11 @@ func (gg *galleryGorm) Create(gallery *Gallery) error {
 
 func (gg *galleryGorm) Update(gallery *Gallery) error {
 	return gg.db.Save(gallery).Error
+}
+
+func (gg *galleryGorm) Delete(id uint) error {
+	gallery := Gallery{Model: gorm.Model{ID: id}}
+	return gg.db.Delete(&gallery).Error
 }
 
 func (gg *galleryGorm) ByID(id uint) (*Gallery, error) {
