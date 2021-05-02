1815342b5a8d066d2d9a5cddffab19ac2326b376
Vitaly Sizikov <606er.54rus@gmail.com>
2021-05-02 22:38:42 +0700

    Created the galleries controller update action

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"learn-web-dev-with-go/context"
 	"learn-web-dev-with-go/models"
 	"learn-web-dev-with-go/views"
@@ -62,6 +63,32 @@ func (g *Galleries) Edit(w http.ResponseWriter, r *http.Request) {
 	var vd views.Data
 	vd.Yield = gallery
 	g.EditView.Render(w, vd)
+}
+
+// POST /galleries/:id/update
+func (g *Galleries) Update(w http.ResponseWriter, r *http.Request) {
+	gallery, err := g.galleryByID(w, r)
+	if err != nil {
+		return
+	}
+	user := context.User(r.Context())
+	if gallery.UserID != user.ID {
+		http.Error(w, "Gallery not found", http.StatusNotFound)
+		return
+	}
+	var vd views.Data
+	vd.Yield = gallery
+	var form GalleryForm
+	if err := parseForm(r, &form); err != nil {
+		log.Println(err)
+		vd.SetAlert(err)
+		g.EditView.Render(w, vd)
+		return
+	}
+	gallery.Title = form.Title
+	// g.gs.Update
+	fmt.Fprintln(w, gallery)
+	// g.EditView.Render(w, vd)
 }
 
 // POST /galleries
--- main.go
+++ main.go
@@ -52,6 +52,7 @@ func main() {
 	r.Handle("/galleries/new", requireUserMw.Apply(galleriesC.New)).Methods("GET")
 	r.HandleFunc("/galleries", requireUserMw.ApplyFn(galleriesC.Create)).Methods("POST")
 	r.HandleFunc("/galleries/{id:[0-9]+}/edit", requireUserMw.ApplyFn(galleriesC.Edit)).Methods("GET")
+	r.HandleFunc("/galleries/{id:[0-9]+}/update", requireUserMw.ApplyFn(galleriesC.Update)).Methods("POST")
 
 	r.HandleFunc("/galleries/{id:[0-9]+}", galleriesC.Show).Methods("GET").Name(controllers.ShowGallery)
 
